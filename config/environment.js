b23dfca9d2a00bf8b7d13c7c6dd77bbbdbe5ec66
Ilya Radchenko <knownasilya@gmail.com>
2019-06-03 12:20:27 -0400

    update host

--- config/environment.js
+++ config/environment.js
@@ -71,7 +71,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
-    ENV.blog.host = 'https://knownasilya.github.io/ilyaradchenko.com';
+    ENV.blog.host = 'https://knownasilya.github.io';
   }
 
   return ENV;
