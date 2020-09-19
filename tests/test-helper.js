04425b13e364d36194b235d06da01d5ef9e605a2
Ilya Radchenko <knownasilya@gmail.com>
2020-09-19 17:22:15 -0400

    v3.10.1...v3.21.2

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'super-blog/app';
+import config from 'super-blog/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
